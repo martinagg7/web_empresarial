b4104feeb4a81c64fc68bc312df95ff5fbed7abc
Martina Garcia <martinagg@myuax.com>
2024-11-22 15:36:52 +0100

    prueba con test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+
+
+class CoreURLTests(TestCase):
+    def test_home_url(self):
+        url = reverse('home') 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200) #comprobar que devuelva la pag
