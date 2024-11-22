85f3ac5b4740f67db295d0fa55b858acd9bda15c
Martina Garcia <martinagg@myuax.com>
2024-11-22 16:14:49 +0100

    templates-core

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('products/',views.products,name='products'),
+    path('sample/',views.sample,name='sample'),
+    path('services/',views.services,name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Bienvenido a Core</h1>')
+    return render(request,'core/home.html')
+def products(request):
+    return render(request,'core/products.html')
+def sample(request):
+    return render(request,'core/sample.html')
+def services(request):
+    return render(request, 'core/services.html')
+def about(request):
+    return render(request, 'core/about.html')
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
